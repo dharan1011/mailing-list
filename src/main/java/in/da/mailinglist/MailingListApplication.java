9fde99fbccb49eb37fc75410dfc631dcd1a380eb
Dharan Aditya <dharan.aditya@outlook.com>
2020-11-15 21:22:12 +0530

    Initial Commit

---
+++ src/main/java/in/da/mailinglist/MailingListApplication.java
@@ -0,0 +1,13 @@
+package in.da.mailinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailingListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/da/mailinglist/MailingListApplicationTests.java
@@ -0,0 +1,13 @@
+package in.da.mailinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
